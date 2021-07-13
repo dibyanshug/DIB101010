63bebee56776097cb56d4cd5584ad70c8a7706d2
dibyanshug <86604110+dibyanshug@users.noreply.github.com>
2021-07-13 17:09:45 +0530

    second commit

---
+++ main.c
@@ -0,0 +1,250 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<windows.h>
+#include<unistd.h>
+int i,j;
+int main_exit;
+void menu();
+struct date{
+    int month,day,year;
+
+    };
+struct {
+
+    char name[60];
+    int acc_no,age;
+    char address[60];
+    double phone;
+    struct date dob;
+    struct date deposit;
+    }add,upd,check,rem,transaction;
+
+void fordelay(int j)
+{   int i,k;
+    for(i=0;i<j;i++)
+         k=i;
+}
+
+void new_acc()
+
+{
+    int choice;
+    FILE *ptr;
+
+    ptr=fopen("record.dat","a+");
+    account_no:
+    system("cls");
+        printf("\n\n\nEnter today's date(mm/dd/yyyy):");
+    scanf("%d/%d/%d",&add.deposit.month,&add.deposit.day,&add.deposit.year);
+    printf("\nEnter the account number:");
+    scanf("%d",&check.acc_no);
+    while(fscanf(ptr,"%d %s %d/%d/%d %d %s %lf\n",&add.acc_no,add.name,&add.dob.month,&add.dob.day,&add.dob.year,&add.age,add.address,&add.phone)!=EOF)
+    {
+        if (check.acc_no==add.acc_no)
+            {printf("Account no. already in use!");
+            fordelay(1000000000);
+                goto account_no;
+
+            }
+    }
+    add.acc_no=check.acc_no;
+        printf("\nEnter the name:");
+    scanf("%s",add.name);
+    printf("\nEnter the date of birth(mm/dd/yyyy):");
+    scanf("%d/%d/%d",&add.dob.month,&add.dob.day,&add.dob.year);
+    printf("\nEnter the age:");
+    scanf("%d",&add.age);
+    printf("\nEnter the address:");
+    scanf("%s",add.address);
+    printf("\nEnter the phone number: ");
+    scanf("%lf",&add.phone);
+
+        fprintf(ptr,"%d %s %d/%d/%d %d %s %lf\n",add.acc_no,add.name,add.dob.month,add.dob.day,add.dob.year,add.age,add.address,add.phone);
+
+
+    fclose(ptr);
+    printf("\nAccount created successfully!");
+    add_invalid:
+    printf("\n\n\n\t\tEnter 1 to go to the main menu and 0 to exit:");
+    scanf("%d",&main_exit);
+    system("cls");
+    if (main_exit==1)
+        menu();
+    
+    else
+        {
+            printf("\nInvalid!\a");
+            goto add_invalid;
+        }
+}
+void view_list()
+{
+    FILE *view;
+    view=fopen("record.dat","r");
+    int test=0;
+    system("cls");
+    printf("\nACC. NO.\tNAME\t\t\tADDRESS\t\t\tPHONE\n");
+
+    while(fscanf(view,"%d %s %d/%d/%d %d %s %s %lf %s %f %d/%d/%d",&add.acc_no,add.name,&add.dob.month,&add.dob.day,&add.dob.year,&add.age,add.address,&add.phone)!=EOF)
+       {
+           printf("\n%6d\t %10s\t\t\t%10s\t\t%.0lf",add.acc_no,add.name,add.address,add.phone);
+           test++;
+       }
+
+    fclose(view);
+    if (test==0)
+        {   system("cls");
+            printf("\nNO RECORDS!!\n");}
+
+    view_list_invalid:
+     printf("\n\nEnter 1 to go to the main menu and 0 to exit:");
+        scanf("%d",&main_exit);
+        system("cls");
+        if (main_exit==1)
+            menu();
+        else
+        {
+            printf("\nInvalid!\a");
+            goto view_list_invalid;
+        }
+}
+void edit(void)
+{
+    int choice,test=0;
+    FILE *old,*newrec;
+    old=fopen("record.dat","r");
+    newrec=fopen("new.dat","w");
+
+    printf("\nEnter the account no. of the customer whose info you want to change:");
+    scanf("%d",&upd.acc_no);
+    while(fscanf(old,"%d %s %d/%d/%d %d %s %s %lf %s %f %d/%d/%d",&add.acc_no,add.name,&add.dob.month,&add.dob.day,&add.dob.year,&add.age,add.address,&add.phone)!=EOF)
+    {
+        if (add.acc_no==upd.acc_no)
+        {   test=1;
+            printf("\nWhich information do you want to change?\n1.Address\n2.Phone\n\nEnter your choice(1 for address and 2 for phone):");
+            scanf("%d",&choice);
+            system("cls");
+            if(choice==1)
+                {printf("Enter the new address:");
+                scanf("%s",upd.address);
+                fprintf(newrec,"%d %s %d/%d/%d %d %s %s %lf %s %f %d/%d/%d\n",add.acc_no,add.name,add.dob.month,add.dob.day,add.dob.year,add.age,upd.address,add.phone);
+                system("cls");
+                printf("Changes saved!");
+                }
+            else if(choice==2)
+                {
+                    printf("Enter the new phone number:");
+                scanf("%lf",&upd.phone);
+                fprintf(newrec,"%d %s %d/%d/%d %d %s %s %lf %s %f %d/%d/%d\n",add.acc_no,add.name,add.dob.month,add.dob.day,add.dob.year,add.age,add.address,upd.phone);
+                system("cls");
+                printf("Changes saved!");
+                }
+
+        }
+        else
+            fprintf(newrec,"%d %s %d/%d/%d %d %s %s %lf %s %f %d/%d/%d\n",add.acc_no,add.name,add.dob.month,add.dob.day,add.dob.year,add.age,add.address,add.phone);
+    }
+    fclose(old);
+    fclose(newrec);
+    remove("record.dat");
+    rename("new.dat","record.dat");
+
+if(test!=1)
+        {   system("cls");
+            printf("\nRecord not found!!\a\a\a");
+            edit_invalid:
+              printf("\nEnter 0 to try again,1 to return to main menu and 2 to exit:");
+              scanf("%d",&main_exit);
+              system("cls");
+                 if (main_exit==1)
+
+                    menu();
+                
+                else if(main_exit==0)
+                    edit();
+                else
+                    {printf("\nInvalid!\a");
+                    goto edit_invalid;}
+        }
+    else
+        {printf("\n\n\nEnter 1 to go to the main menu and 0 to exit:");
+        scanf("%d",&main_exit);
+        system("cls");
+        if (main_exit==1)
+            menu();
+        
+        }
+}
+    
+
+void menu(void)
+{  
+    int choice;
+    system("cls");
+    system("color 8");
+    printf("\n\n\t\t\tCOUPON GENERATING MACHINE");
+    printf("\n\n\n\t\t\t\xB2\xB2\xB2\xB2\xB2\xB2\xB2 WELCOME FOODIE \xB2\xB2\xB2\xB2\xB2\xB2\xB2");
+    printf("\n\n\t\t1.REGISTER YOURSELF\n\t\t2.UPDATE ACCOUNT DETAILS\n\t\t3.CHECK EXISTING ACCOUNT DETAILS\n\t\t4.Exit\n\n\n\n\n\t\t Enter your choice:");
+    scanf("%d",&choice);
+
+    system("cls");
+    switch(choice)
+    {
+        case 1:new_acc();
+        break;
+        case 2:edit();
+        break;
+    }
+
+}
+int main()
+{
+    char pass[20],password[20]="Cocacola";
+    int i=0;
+    printf("\n\n\t\tEnter the Cocacola to login:");
+    scanf("%s",pass);
+    /*do
+    {
+    //if (pass[i]!=13&&pass[i]!=8)
+        {
+            printf("*");
+            pass[i]=getch();
+            i++;
+        }
+    }while (pass[i]!=13);
+    pass[10]='\0';*/
+    if (strcmp(pass,password)==0)
+        {printf("\n\nPassword Match!\nLOADING");
+        for(i=0;i<=6;i++)
+        {
+            fordelay(100000000);
+            printf(".");
+        }
+                system("cls");
+            menu();
+        }
+    else
+        {   printf("\n\nWrong password!!\a\a\a");
+            login_try:
+            printf("\nEnter 1 to try again and 0 to exit:");
+            scanf("%d",&main_exit);
+            if (main_exit==1)
+                    {
+
+                        system("cls");
+                        main();
+                    }
+
+            else if (main_exit==0)
+                    {
+                    system("cls");
+                    }
+            else
+                    {printf("\nInvalid!");
+                    fordelay(1000000000);
+                    system("cls");
+                    goto login_try;}
+
+        }
+        return 0;
+}
